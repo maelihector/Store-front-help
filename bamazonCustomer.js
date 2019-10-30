2390223c546e6d482304e2948255c27093e7058d
mhector <maelihector@gmail.com>
2019-10-30 11:39:50 -0500

    Add javaScript to update 'product_sales' with each product purchase. Use 'table' npm package to create table for products data. Edit comments. Check javaScript on jshint.com and make appropriate changes.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,112 +1,138 @@
-// Require needed npm packages and bring into scope of js file.
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-// Connect to mySQl database with necessary credentials and specify database name.
-var con = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'bamazon'
+// Require packages and bring into scope
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const {
+  table
+} = require('table');
+
+// Connect to the mySQl database with necessary credentials and specify database name
+const con = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'bamazon'
 });
-// Return the error description if there is an error when connecting to the database.
-con.connect(function (err) {
-    if (err) throw err;
+
+// Return the error description if there is an error when connecting to the database
+con.connect(err => {
+  if (err) throw err;
+  // Initialize APP
+  startCustomer();
 });
-// Function to display products for sale to customer at initialization of application.
+
+// Function to display products for sale to customer at initialization of application
 function startCustomer() {
-    con.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        //console.log(res); // Check for appropriate response
-        console.log(`
+  con.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    // Create empty array to hold table rows
+    let tableRows = [];
+    //console.log(res); // Check for appropriate response
+    console.log(`
 
-                            =================================================================
+                  ~~*~~ WELCOME TO BAMAZON ~~*~~
 
-                                                ~~*~~ WELCOME TO BAMAZON ~~*~~
-
-               PLEASE LOOK THROUGH OUR PRODUCT LIST AND NOTE THE 'Product ID' OF THE ITEM YOU WISH TO PURCHASE
-            
-                            =================================================================
-            `);
-        // Loop through the 'products',
-        for (var i = 0; i < res.length; i++) { 
-            console.log(`
-            Name: ${res[i].product_name}
-            Price: $${res[i].price}
-            Product ID: ${res[i].item_id}`) // and only return product name, price, and item_id with their respective values.
+    `);
+    // Loop through returned results and fetch data to build each row
+    for (let i = 0; i < res.length; i++) {
+      // Create empty row array
+      let row = [];
+      row.push(res[i].item_id, res[i].product_name, "$" + res[i].price.toFixed(2));
+      tableRows.push(row);
+    }
+    // Create undefined data and output instances
+    let data,
+      output;
+    // Create headear row to label each column
+    let headerRow = ['Product ID', 'Name', 'Price'];
+    // Add header row to front of data rows
+    tableRows.unshift(headerRow);
+    // Give 'data' the value of 'tableRows'
+    data = tableRows;
+    // Give 'output' the value of function table() with 'data' as argument to build table using 'table' npm package
+    output = table(data);
+    // Log the table
+    console.log(`${output}
+    
+    `);
+    // Add prompt messages to gather the product and amount the customer wants to purchase
+    inquirer.prompt([{
+        type: "input",
+        name: "id",
+        message: "What is the ID of the product you would like to purchase?",
+        // Make sure the input is a number and that it matches an actual product item_id
+        validate: function (value) {
+          if (isNaN(value) === false && parseInt(value) <= res.length && parseInt(value) > 0) {
+            return true;
+          } else {
+            return false;
+          }
         }
-        console.log(`
-        =================================================================
-                            END OF PRODUCTS LIST
-        =================================================================
-        `);
-        // Add prompt messages to gather the product and amount of product the customer wants to purchase.
-        inquirer.prompt([{
-                type: "input",
-                name: "id",
-                message: "What is the ID of the product you would like to purchase?",
-                validate: function (value) { // Make sure the input is a number and that it matches one of our 'item_id's.
-                    if (isNaN(value) === false && parseInt(value) <= res.length && parseInt(value) > 0) {
-                        return true;
-                    } else {
-                        return false;
-                    }
-                }
-            },
-            {
-                type: "input",
-                name: "qty",
-                message: "How many would you like to purchase?",
-                validate: function (value) {
-                    if (isNaN(value)) { // Make sure that the input is a number value.
-                        return false;
-                    } else {
-                        return true;
-                    }
-                }
-            }
-            // Funtion to check if Bamazon has enough of the product to meet the customer's request. 
-        ]).then(function (input) {
-            var itemToPurchase = (input.id) - 1;
-            var purchaseQuantity = parseInt(input.qty);
-            var currentQuantity = (res[itemToPurchase].stock_quantity);
-            var totalCost = parseFloat(((res[itemToPurchase].price) * purchaseQuantity).toFixed(2)); // Gather totol cost to customer by grabing the 'price' of the input.id, and and multiplying it by the purchase quantity, and returning a dollar amount.
-            var productName = (res[itemToPurchase].product_name);
-            // Check if Bamazon's stock_quantity value is greater than or equal to the customer's quantity request.
-            if (currentQuantity >= purchaseQuantity) {
-                con.query("UPDATE Products SET ? WHERE ?", [{
-                        // If so, stock_quantity by subtracting the customer's purchase quantity from the table's stock_quantity.
-                        stock_quantity: (currentQuantity - purchaseQuantity)
-                    },
-                    {
-                        item_id: (input.id)
-                    }
-                ], function (err, result) {
-                    if (err) throw err;
-                    // Show the customer the total cost of their purchase.
-                    console.log(`
-                                ~~Your order went through!~~
+      },
+      {
+        type: "input",
+        name: "qty",
+        message: "How many would you like to purchase?",
+        validate: function (value) {
+          // Make sure that the input is a number value
+          if (isNaN(value)) {
+            return false;
+          } else {
+            return true;
+          }
+        }
+      }
 
-                              Your total today was: $${totalCost} 
+      // Funtion to check if Bamazon has enough of the product to meet the customer's request
+    ]).then((input) => {
+      // Fetch product data
+      let itemToPurchase = (input.id) - 1;
+      let productName = (res[itemToPurchase].product_name);
+      let purchaseQuantity = parseInt(input.qty);
+      let currentQuantity = (res[itemToPurchase].stock_quantity);
+      let currentSales = (res[itemToPurchase].product_sales);
+      let currentTotalSold = (res[itemToPurchase].products_sold);
+      // Gather totol cost to customer by grabing the 'price' and multiplying it by the purchase quantity
+      let totalCost = parseFloat(((res[itemToPurchase].price) * purchaseQuantity).toFixed(2));
+      // If Bamazon's stock_quantity value is greater than or equal to the customer's quantity request, allow purchase to go through
+      if (currentQuantity >= purchaseQuantity) {
+        con.query("UPDATE Products SET ? WHERE ?", [{
+            stock_quantity: (currentQuantity - purchaseQuantity),
+            product_sales: (currentSales + totalCost),
+            products_sold: (currentTotalSold + purchaseQuantity)
+          },
+          {
+            item_id: (input.id)
+          }
+        ], function (err, result) {
+          if (err) throw err;
+          // Show the customer the total cost of their purchase
+          console.log(`
+                                      *********
 
-                        ${purchaseQuantity} of ${productName}(s)
+                              ~~Your order went through!~~
 
-                       will be shipped to you in 3-5 business days.
+                              Your total today was: $${totalCost}
 
-                                        *********
+            ${purchaseQuantity} of ${productName}(s) will be shipped to you in 3-5 business days.
 
-                                THANK YOU FOR YOUR BUSINESS!
-                    `);
-                    con.end();
-                });
-            } else {
-                console.log(`
-                Sorry, we have insufficient quantity in stock, 
-                here is the quantity we currently have: ${currentQuantity}
-                `);
-                con.end();
-            }
+                                      *********
+
+                            THANK YOU FOR YOUR BUSINESS!
+          `);
+          con.end();
         });
-    })  
+      } else {
+        console.log(`
+                              *********
+
+        Sorry, we have ${currentQuantity} left of ${productName} in stock.
+
+                              *********
+
+                `);
+        con.end();
+      }
+    });
+  });
 }
-startCustomer();
