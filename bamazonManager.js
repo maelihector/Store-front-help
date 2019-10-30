15b6dc3e063a78abdaabe49ada20fd434230973a
mhector <maelihector@gmail.com>
2019-10-30 12:58:06 -0500

    Use 'table' npm package to create table for database data for manager to view. Edit comments. Check javaScript on jshint.com and make appropriate changes

--- bamazonManager.js
+++ bamazonManager.js
@@ -1,227 +1,263 @@
-// Require needed npm packages and bring into scope of js file.
-var mysql = require('mysql');
-var inquirer = require('inquirer');
-// Connect to mySQl database with necessary credentials and specify database name.
-var con = mysql.createConnection({
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: '',
-    database: 'bamazon'
+// Require packages and bring into scope
+const mysql = require('mysql');
+const inquirer = require('inquirer');
+const {
+  table
+} = require('table');
+
+// Connect to mySQl database with necessary credentials and specify database name
+const con = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: '',
+  database: 'bamazon'
 });
-// Return the error description if there is an error when connecting to the database.
-con.connect(function (err) {
-    if (err) throw err;
+
+// Return the error description if there is an error when connecting to the database
+con.connect(err => {
+  if (err) throw err;
+  // Initialize APP
+  startManager();
 });
-// Function to display prompt at initialization of application.
+
+// Function to display prompt at initialization of application
 function startManager() {
-    inquirer.prompt([{
-        type: "list",
-        name: "action",
-        message: "Manager Actions:",
-        choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product"]
-    }]).then(function (choice) {
-        switch (choice.action) {
-            case "View Products for Sale":
-                viewTotalInventory();
-                break;
-            case "View Low Inventory":
-                viewLowInventory();
-                break;
-            case "Add to Inventory":
-                addToInventory();
-                break;
-            case "Add New Product":
-                addProduct();
-                break;
-        }
-    });
+  inquirer.prompt([{
+    type: "list",
+    name: "action",
+    message: "Manager Actions:",
+    choices: ["View Products for Sale", "View Low Inventory", "Add to Inventory", "Add New Product", "Exit"]
+  }]).then(choice => {
+    switch (choice.action) {
+      case "View Products for Sale":
+        viewTotalInventory();
+        break;
+      case "View Low Inventory":
+        viewLowInventory();
+        break;
+      case "Add to Inventory":
+        addToInventory();
+        break;
+      case "Add New Product":
+        addProduct();
+        break;
+      case "Exit":
+        exitApp();
+        break;
+    }
+  });
 }
-// Function to view total inventory in 'products' table.
+
+// Function to view total inventory
 function viewTotalInventory() {
-    con.query("SELECT * FROM products", function (err, res) {
-        if (err) throw err;
-        //console.log(res); // Check for appropriate response
-        console.log(`
+  con.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    console.log(`
                 
-                =================================================================
-                                    BAMAZON'S TOTAL INVENTORY
-                =================================================================
+                                  BAMAZON'S TOTAL INVENTORY
+
                 `);
-        for (var i = 0; i < res.length; i++) { // Loop through the 'products',
-            console.log(`
-                item_id:            ${res[i].item_id}
-                product_name:       ${res[i].product_name}
-                price:              $${res[i].price}
-                stock_quantity:     ${res[i].stock_quantity}`) // and return all current inventory.
-        }
-        console.log(`
-                =================================================================
-                                        END OF INVENTORY
-                =================================================================
-            `);
-        startManager();
-    })
+
+    // Call function to build table
+    buildTable(res);
+
+    // Take manager back to prompts
+    startManager();
+  });
 }
-// Function to view inventory with quantity < 5 in the 'products' table.
+
+// Function to view inventory with stock_quantity less than or equal to 5
 function viewLowInventory() {
-    con.query('SELECT * FROM products', function (err, res) {
-        if (err) throw err;
-        console.log(`
+  con.query('SELECT * FROM products WHERE stock_quantity <= 5', function (err, res) {
+    if (err) throw err;
+    console.log(`
                 
-        =================================================================
-                            BAMAZON'S LOW INVENTORY
-        =================================================================
+                    BAMAZON'S LOW INVENTORY
         `);
-        // Loop through the 'products',
-        for (var i = 0; i < res.length; i++) {
-            if (res[i].stock_quantity <= 5) { // and only return those products with a stock_quantity < 5.
-                console.log(`
-                item_id:            ${res[i].item_id}
-                product_name:       ${res[i].product_name}
-                price:              $${res[i].price}
-                stock_quantity:     ${res[i].stock_quantity}`)
-            }
-        }
-        console.log(`
-        =================================================================
-                            END OF LOW INVENTORY
-        =================================================================
-        `);
-        startManager();
-    });
+
+    // Call function to build table
+    buildTable(res);
+
+    // Take manager back to prompts
+    startManager();
+  });
 }
-// Function to add stock_quantity amount to any product in the 'products' table.
+
+// Function to add stock_quantity amount to any product
 function addToInventory() {
-    con.query('SELECT * FROM products', function (err, res) {
-        if (err) throw err;
-        // Create empty var to hold the list of products.
-        var itemArray = [];
-        // Push each item into the itemArray var.
-        for (var i = 0; i < res.length; i++) {
-            itemArray.push(res[i].product_name);
+  con.query('SELECT * FROM products', function (err, res) {
+    if (err) throw err;
+    // Create empty var to hold the list of products
+    let itemsArray = [];
+    // Push each product into itemsArray
+    for (let i = 0; i < res.length; i++) {
+      itemsArray.push(res[i].product_name);
+    }
+    // List all products so manager can choose which product, and then ask quantity of inventory to add
+    inquirer.prompt([{
+      type: "list",
+      name: "product",
+      choices: itemsArray,
+      message: "Select which product you want to add inventory to:"
+    }, {
+      type: "input",
+      name: "qty",
+      message: "How much inventory would you like to add?",
+      validate: function (value) {
+        if (isNaN(value) === false) {
+          return true;
+        } else {
+          return false;
         }
-        inquirer.prompt([{
-            type: "list",
-            name: "product",
-            choices: itemArray,
-            message: "Select which product you want to add inventory to:"
-        }, {
-            type: "input",
-            name: "qty",
-            message: "How much inventory would you like to add?",
-            validate: function (value) { // Make sure input value is a number.
-                if (isNaN(value) === false) {
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }]).then(function (ans) {
-            // Loop through results
-            for (var i = 0; i < res.length; i++) {
-                var productName = (res[i].product_name);
-                var currentQuantity = res[i].stock_quantity;
-                // if Manager input equals a value of product_name,
-                if (productName === ans.product) {
-                    con.query('UPDATE products SET ? WHERE ?', [{
-                            // update said product's stock_quantity by adding Manager's input value.
-                            stock_quantity: currentQuantity + parseInt(ans.qty)
-                        },
-                        {
-                            product_name: ans.product
-                        }
-                    ], function (err, res) {
-                        if (err) throw err;
-                        console.log(`
+      }
+    }]).then((ans) => {
+      // Query database to update new inventory
+      con.query('UPDATE products SET stock_quantity=stock_quantity+' + ans.qty + ' WHERE ?', [{
+        product_name: ans.product
+      }], function (err, result) {
+        if (err) throw err;
+        console.log(`
         =================================================================
 
-        Rows affected: ${res.affectedRows}
+        Rows affected: ${result.affectedRows}
         
-        The stock_quantity of '${ans.product}' was updated.
+        The stock_quantity of '${ans.product}' was updated by ${ans.qty}.
 
         =================================================================
                         `);
-                        startManager();
-                    });
-                }
-            }
-        })
+        // Take manager back to prompts
+        startManager();
+      });
     });
+  });
 }
-//allows manager to add a completely new product to store
+
+// Function that allows manager to add a completely new product to Bamazon
 function addProduct() {
-    console.log(`You are adding a new and unique product to 'products' table in 'bamazon' database.`);
-    var departments = [];
-    // Grab name of department_names for Manager to chose from.
-    con.query('SELECT * FROM products', function (err, res) {
-        if (err) throw err;
-        for (var i = 0; i < res.length; i++) {
-            departments.push(res[i].department_name);
-        }
-    })
-    inquirer.prompt([{
-        type: "input",
-        name: "item",
-        message: "Input name of item:",
-        validate: function (value) {
-            if (value) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }, {
-        type: "list",
-        name: "department",
-        message: "Choose the department the item belongs to:",
-        choices: departments
-    }, {
-        type: "input",
-        name: "price",
-        message: "Input the price of the item:",
-        validate: function (value) {
-            if (isNaN(value) === false) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }, {
-        type: "input",
-        name: "quantity",
-        message: "Input the stock_quantity of item:",
-        validate: function (value) {
-            if (isNaN(value) == false) {
-                return true;
-            } else {
-                return false;
-            }
-        }
-    }]).then(function (ans) {
-        console.log(`
-                
-        =================================================================
+  console.log(`You are adding a new and unique product to 'products' table in 'bamazon' database.`);
+  let departments = [];
+  // Grab name of department_names for Manager to chose from
+  con.query('SELECT * FROM departments', function (err, res) {
+    if (err) throw err;
+    for (let i = 0; i < res.length; i++) {
+      departments.push(res[i].department_name);
+    }
+  });
+  inquirer.prompt([{
+    type: "input",
+    name: "item",
+    message: "Input name of item:",
+    validate: function (value) {
+      if (value) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }, {
+    type: "list",
+    name: "department",
+    message: "Choose the department the item belongs to:",
+    choices: departments
+  }, {
+    type: "input",
+    name: "price",
+    message: "Input the price of the item:",
+    validate: function (value) {
+      if (isNaN(value) === false) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }, {
+    type: "input",
+    name: "quantity",
+    message: "Input the stock_quantity of item:",
+    validate: function (value) {
+      if (isNaN(value) == false) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }]).then(ans => {
+    con.query('INSERT INTO products SET ?', {
+      product_name: ans.item,
+      department_name: ans.department,
+      price: ans.price,
+      stock_quantity: ans.quantity
+    }, function (err, res) {
+      if (err) throw err;
+      console.log(`
+
+      =================================================================
             YOU ADDED THE FOLLOWING RECORD TO 'PRODUCTS' TABLE:
-        =================================================================
-        `);
-        con.query('INSERT INTO products SET ?', {
-            product_name: ans.item,
-            department_name: ans.department,
-            price: ans.price,
-            stock_quantity: ans.quantity
-        }, function (err, res) {
-            if (err) throw err;
-            console.log(`
-            product_name:       ${ans.item}
-            department_name:    ${ans.department}
-            price:              ${ans.price}
-            stock_quantity:     ${ans.quantity}
+      =================================================================
+
+                product_name:       ${ans.item}
+                department_name:    ${ans.department}
+                price:              ${ans.price}
+                stock_quantity:     ${ans.quantity}
 
             `);
-            startManager();
-        })
+      // Take manager back to prompts
+      startManager();
     });
-    
+  });
 }
-startManager();
+
+// Function to build table
+function buildTable(res) {
+  // Create empty array to hold table rows
+  let tableRows = [];
+
+  // Loop through the products and fetch data to build each row
+  for (let i = 0; i < res.length; i++) {
+    // Create empty row array
+    let row = [];
+    row.push(res[i].item_id, res[i].product_name, "$" + res[i].price.toFixed(2), res[i].stock_quantity, res[i].products_sold);
+    tableRows.push(row);
+  }
+  // Create undefined data and output instances
+  let config, data,
+    output;
+
+  // Create headear row to label each column
+  let headerRow = ['Product ID', 'Name', 'Price', 'Stock Quantity', 'Quantity Sold'];
+
+  // Add header row to front of data rows
+  tableRows.unshift(headerRow);
+
+  // Give 'data' the value of 'tableRows'
+  data = tableRows;
+
+  // Format cols
+  config = {
+    columns: {
+      0: {
+        alignment: 'center'
+      },
+      3: {
+        alignment: 'center'
+      },
+      4: {
+        alignment: 'center'
+      }
+    }
+  };
+
+  // Give 'output' the value of function table() with 'data' as argument to build table using 'table' npm package
+  output = table(data, config);
+
+  // Log the table
+  console.log(`${output}
+      
+      `);
+}
+
+// Function to close connection
+function exitApp() {
+  con.end();
+}
